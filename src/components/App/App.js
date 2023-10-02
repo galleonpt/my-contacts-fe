f2927e3a48b08abd72645d2e0ae8921b653cbbcc
galleonpt <jose15.luis00@gmail.com>
2023-10-03 00:32:09 +0100

    add header component

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import { ThemeProvider } from 'styled-components';
+import defaultTheme from '../../assets/styles/themes/default';
+import GlobalStyles from '../../assets/styles/global';
+import { Container } from './styles';
+import Header from '../Header/Header';
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+
+      <Container>
+        <Header />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
