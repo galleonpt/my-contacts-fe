da8d8fdf386525c8a092bd42730c182726e47de7
galleonpt <jose15.luis00@gmail.com>
2023-11-03 17:46:48 +0000

    feat: contacts list

--- src/components/ContactsList/index.js
+++ src/components/ContactsList/index.js
@@ -1,4 +1,9 @@
-import { Container, Header } from './styles';
+import {
+  Container, Header, ListContainer, Card,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
 
 export default function ContactsList() {
   return (
@@ -7,6 +12,77 @@ export default function ContactsList() {
         <strong>3 contactos</strong>
         <a href="/">Novo contacto</a>
       </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email</span>
+            <span>mobile number</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button"><img src={trash} alt="Delete" /></button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email</span>
+            <span>mobile number</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button"><img src={trash} alt="Delete" /></button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email</span>
+            <span>mobile number</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button"><img src={trash} alt="Delete" /></button>
+          </div>
+        </Card>
+
+      </ListContainer>
     </Container>
   );
 }
