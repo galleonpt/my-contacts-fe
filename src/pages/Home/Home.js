22b67869c63c5f063ea0733673452f23d8b8421b
galleonpt <jose15.luis00@gmail.com>
2023-11-14 18:05:59 +0000

    refactor: header && feat: home page

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,3 +1,94 @@
-const Home = () => 'Home';
+import {
+  Container, Header, ListContainer, Card, InputSearchContainer,
+} from './styles';
+import arrow from '../../assets/images/icons/arrow.svg';
+import edit from '../../assets/images/icons/edit.svg';
+import trash from '../../assets/images/icons/trash.svg';
+
+function Home() {
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar contacto ..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contactos</strong>
+        <a href="/">Novo contacto</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email</span>
+            <span>mobile number</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button"><img src={trash} alt="Delete" /></button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email</span>
+            <span>mobile number</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button"><img src={trash} alt="Delete" /></button>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Nome</strong>
+              <small>instagram</small>
+            </div>
+
+            <span>email</span>
+            <span>mobile number</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+
+            <button type="button"><img src={trash} alt="Delete" /></button>
+          </div>
+        </Card>
+
+      </ListContainer>
+    </Container>
+  );
+}
 
 export default Home;
