2d8c4fb45f96e526f429f45523595db28d5e82fb
galleonpt <jose15.luis00@gmail.com>
2023-11-15 23:24:44 +0000

    feat: input && select component

--- src/pages/NewContact/NewContact.jsx
+++ src/pages/NewContact/NewContact.jsx
@@ -1,10 +1,23 @@
 import PageHeader from '../../components/PageHeader/PageHeader';
+import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 function NewContact() {
   return (
-    <PageHeader
-      title="Novo contacto"
-    />
+    <>
+      <PageHeader
+        title="Novo contacto"
+      />
+
+      <Input placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+        <option value="123">Instagram</option>
+      </Select>
+
+    </>
   );
 }
 
