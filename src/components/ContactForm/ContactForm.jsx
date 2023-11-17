866e8b597f6c9023bcc1505ae6bec0960d884315
galleonpt <jose15.luis00@gmail.com>
2023-11-17 18:20:04 +0000

    input controllers

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Form, ButtonContainer } from './styles';
 import FormGroup from '../FormGroup/FormGroup';
@@ -6,30 +7,57 @@ import Select from '../Select';
 import Button from '../Button';
 
 function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(
+      name,
+      email,
+      phone,
+      category,
+    );
+  }
+
   return (
-    <Form>
+    <Form onSubmit={(event) => handleSubmit(event)}>
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
-
-      <FormGroup
-        error="Formato de email inválido"
-      >
-        <Input placeholder="Email" error />
-      </FormGroup>
-
-      <FormGroup>
-        <Input placeholder="Telemovel" />
+        <Input
+          value={name}
+          placeholder="Nome"
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
 
       <FormGroup>
-        <Select>
+        <Input
+          value={email}
+          placeholder="Email"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Input
+          value={phone}
+          placeholder="Telemovel"
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
           <option value="instagram">Instagram</option>
         </Select>
       </FormGroup>
 
       <ButtonContainer>
-
         <Button type="submit">
           {buttonLabel}
         </Button>
