cb949d2357c570398f700e106b65cff65d4748c0
galleonpt <jose15.luis00@gmail.com>
2023-11-29 19:05:06 +0000

    feat: contacts search

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -10,6 +10,11 @@ import trash from '../../assets/images/icons/trash.svg';
 function Home() {
   const [contacts, setContacts] = useState([]);
   const [orderBy, setOrderBy] = useState('asc');
+  const [searchName, setSearchName] = useState('');
+
+  const filteredContacts = contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchName.toLowerCase())
+  ));
 
   // ! effects
   useEffect(() => {
@@ -26,52 +31,61 @@ function Home() {
     setOrderBy((prev) => (prev === 'asc' ? 'desc' : 'asc'));
   };
 
+  const handleChangeSearchName = (event) => {
+    setSearchName(event.target.value);
+  };
+
   // ! render
   return (
     <Container>
       <InputSearchContainer>
-        <input type="text" placeholder="Find Contact ..." />
+        <input
+          value={searchName}
+          type="text"
+          placeholder="Find Contact ..."
+          onChange={handleChangeSearchName}
+        />
       </InputSearchContainer>
 
       <Header>
         <strong>
-          {contacts.length}
-          {contacts.length === 1 ? ' contact' : ' contacts'}
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' contact' : ' contacts'}
         </strong>
         <Link to="/new">New contact</Link>
       </Header>
 
-      <ListHeader orderBy={orderBy}>
-        <button type="button" onClick={handleToggleOrderBy}>
-          <span>Name</span>
+      {filteredContacts.length > 0 && (
+        <ListHeader orderBy={orderBy}>
+          <button type="button" onClick={handleToggleOrderBy}>
+            <span>Name</span>
 
-          <img src={arrow} alt="Arrow" />
-        </button>
-      </ListHeader>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </ListHeader>
+      )}
 
-      {
-          contacts.map((contact) => (
-            <Card key={contact.id}>
-              <div className="info">
-                <div className="contact-name">
-                  <strong>{contact.name}</strong>
-                  { contact.category_name && <small>{contact.category_name}</small> }
-                </div>
+      {filteredContacts.map((contact) => (
+        <Card key={contact.id}>
+          <div className="info">
+            <div className="contact-name">
+              <strong>{contact.name}</strong>
+              { contact.category_name && <small>{contact.category_name}</small> }
+            </div>
 
-                <span>{contact.email}</span>
-                <span>{contact.phone}</span>
-              </div>
+            <span>{contact.email}</span>
+            <span>{contact.phone}</span>
+          </div>
 
-              <div className="actions">
-                <Link to={`/edit/${contact.id}`}>
-                  <img src={edit} alt="Edit" />
-                </Link>
+          <div className="actions">
+            <Link to={`/edit/${contact.id}`}>
+              <img src={edit} alt="Edit" />
+            </Link>
 
-                <button type="button"><img src={trash} alt="Delete" /></button>
-              </div>
-            </Card>
-          ))
-        }
+            <button type="button"><img src={trash} alt="Delete" /></button>
+          </div>
+        </Card>
+      ))}
 
     </Container>
   );
