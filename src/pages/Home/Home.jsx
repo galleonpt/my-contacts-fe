df4605f9998996006d79f01ae1fe59b6cd2e6908
galleonpt <jose15.luis00@gmail.com>
2023-12-17 18:53:21 +0000

    feat: show error message on contacts list

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState, useMemo } from 'react';
 import { Link } from 'react-router-dom';
 import {
-  Container, Header, ListHeader, Card, InputSearchContainer,
+  Container, Header, ListHeader, Card, InputSearchContainer, ErrorContainer,
 } from './styles';
 import arrow from '../../assets/images/icons/arrow.svg';
 import edit from '../../assets/images/icons/edit.svg';
 import trash from '../../assets/images/icons/trash.svg';
+import sad from '../../assets/images/sad.svg';
+
 import Loader from '../../components/Loader/Loader';
+import Button from '../../components/Button';
 import ContactsService from '../../services/ContactsService';
 
 function Home() {
@@ -14,6 +17,7 @@ function Home() {
   const [orderBy, setOrderBy] = useState('asc');
   const [searchName, setSearchName] = useState('');
   const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
 
   const filteredContacts = useMemo(() => contacts.filter((contact) => (
     contact.name.toLowerCase().includes(searchName.toLowerCase())
@@ -29,7 +33,7 @@ function Home() {
 
         setContacts(contactsList);
       } catch (error) {
-        console.log(error);
+        setHasError(true);
       } finally {
         setIsLoading(false);
       }
@@ -61,13 +65,26 @@ function Home() {
         />
       </InputSearchContainer>
 
-      <Header>
+      <Header hasError={hasError}>
+        {!hasError && (
         <strong>
           {filteredContacts.length}
           {filteredContacts.length === 1 ? ' contact' : ' contacts'}
         </strong>
+        )}
         <Link to="/new">New contact</Link>
       </Header>
+
+      {hasError && (
+        <ErrorContainer>
+          <img src={sad} alt="sad" />
+
+          <div className="details">
+            <strong>Error getting your contacts!</strong>
+            <Button type="button">Try again</Button>
+          </div>
+        </ErrorContainer>
+      )}
 
       {filteredContacts.length > 0 && (
         <ListHeader orderBy={orderBy}>
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -27,8 +27,11 @@ export const InputSearchContainer = styled.div`
 export const Header = styled.header`
     display: flex;
     align-items: center;
-    justify-content: space-between;
+    justify-content: ${({ hasError }) => (hasError ? 'flex-end' : 'space-between')};
     margin-top: 32px;
+    border-bottom: 2px solid ${({ theme }) => theme.colors.grey[100]};
+    padding-bottom: 16px;
+
 
     strong {
         font-size: 24px;
@@ -120,6 +123,22 @@ export const Card = styled.div`
         button {
             background: transparent;
             border: none;
+        }
+    }
+`;
+
+export const ErrorContainer = styled.div`
+    margin-top: 16px;
+    display: flex;
+    align-items: center;
+    gap: 24px;
+
+    .details {
+        strong {
+            display: block;
+            font-size: 22px;
+            color: ${({ theme }) => theme.colors.danger.main};
+            margin-bottom: 8px
         }
     }
 `;
