14376e697ac83763eefae2ced9df77c47fed5cf5
galleonpt <jose15.luis00@gmail.com>
2023-12-21 20:13:06 +0000

    feat: toast container

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,21 @@
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
 import defaultTheme from '../../assets/styles/themes/default';
 import GlobalStyles from '../../assets/styles/global';
-import { Container } from './styles';
-import Header from '../Header/Header';
+
 import Routes from '../../routes';
+
+import Header from '../Header/Header';
+import ToastContainer from '../Toast/ToastContainer/ToastContainer';
+import { Container } from './styles';
 
 function App() {
   return (
     <BrowserRouter>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyles />
+        <ToastContainer />
 
         <Container>
           <Header />
---
+++ src/components/Toast/ToastContainer/ToastContainer.jsx
@@ -0,0 +1,7 @@
+import { Container } from './styles';
+
+function ToastContainer() {
+  return <Container><h1>ToastContainer</h1></Container>;
+}
+
+export default ToastContainer;
