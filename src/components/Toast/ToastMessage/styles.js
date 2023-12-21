deca0bd44f48ab56ffd70794f725546a9721cf77
galleonpt <jose15.luis00@gmail.com>
2023-12-21 22:15:01 +0000

    feat: toast variants

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,12 +1,26 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const containerVariants = {
+  default: css`
+        background-color: ${({ theme }) => theme.colors.primary.main};
+    `,
+  success: css`
+        background-color: ${({ theme }) => theme.colors.success.main};
+    `,
+  danger: css`
+        background-color: ${({ theme }) => theme.colors.danger.main};
+    `,
+};
 
 export const Container = styled.div`
     padding: 16px 32px;
     border-radius: 4px;
 
     color: #fff;
-    background-color: ${({ theme }) => theme.colors.primary.main};
+
     box-shadow: 0px 20px 20px -16px #00000040;
+
+    ${({ type }) => containerVariants[type] || containerVariants.default};
 
     display: flex;
     justify-content: center;
