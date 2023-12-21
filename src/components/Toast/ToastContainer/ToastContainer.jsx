cc62085e8274d2caf196949146261c434eaf8547
galleonpt <jose15.luis00@gmail.com>
2023-12-21 23:21:24 +0000

    refactor: state to store toast messages

--- src/components/Toast/ToastContainer/ToastContainer.jsx
+++ src/components/Toast/ToastContainer/ToastContainer.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react';
 import ToastMessage from '../ToastMessage/ToastMessage';
 import { Container } from './styles';
 
 function ToastContainer() {
+  const [messages] = useState([
+    { id: Math.random(), type: 'default', text: 'Default message' },
+    { id: Math.random(), type: 'danger', text: 'Danger message' },
+    { id: Math.random(), type: 'success', text: 'Success message' },
+  ]);
+
   return (
     <Container>
-      <ToastMessage text="Default message" />
-      <ToastMessage text="Error toast" type="danger" />
-      <ToastMessage text="Success toast" type="success" />
+      {messages.map((message) => (
+        <ToastMessage
+          key={message.id}
+          type={message.type}
+          text={message.text}
+        />
+      ))}
     </Container>
   );
 }
