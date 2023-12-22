3f0264fea0900342f9ed6842b79ea2ed51717483
galleonpt <jose15.luis00@gmail.com>
2023-12-22 13:51:12 +0000

    refactor: event manager to use Map

--- src/lib/EventManager.js
+++ src/lib/EventManager.js
@@ -1,28 +1,28 @@
 class EventManager {
   constructor() {
-    this.listeners = { };
+    this.listeners = new Map();
   }
 
   on(event, listener) {
-    if (!this.listeners[event]) {
-      this.listeners[event] = [];
+    if (!this.listeners.has(event)) {
+      this.listeners.set(event, []);
     }
 
-    this.listeners[event].push(listener);
+    this.listeners.get(event).push(listener);
   }
 
   emit(event, payload) {
-    if (!this.listeners[event]) {
+    if (!this.listeners.has(event)) {
       return;
     }
 
-    this.listeners[event].forEach((listener) => {
+    this.listeners.get(event).forEach((listener) => {
       listener(payload);
     });
   }
 
   removeLister(event, listenerToRemove) {
-    const eventListeners = this.listeners[event];
+    const eventListeners = this.listeners.get(event);
 
     if (!eventListeners) {
       return;
@@ -30,20 +30,8 @@ class EventManager {
 
     const filteredListeners = eventListeners.filter((listener) => listener !== listenerToRemove);
 
-    this.listeners[event] = filteredListeners;
+    this.listeners.set(event, filteredListeners);
   }
 }
 
 export default new EventManager();
-
-const toastEventMAnager = new EventManager();
-
-const temp = (payload) => console.log(payload);
-
-toastEventMAnager.on('addtoast', temp);
-
-toastEventMAnager.emit('addtoast', { type: 'danger', text: 'Text' });
-
-toastEventMAnager.removeLister('addtoast', temp);
-
-console.log(toastEventMAnager);
