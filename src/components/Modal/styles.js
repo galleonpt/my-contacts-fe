29c85a0d536fc1e6cb6211e7def5c3fa780a43aa
galleonpt <jose15.luis00@gmail.com>
2023-12-26 23:46:47 +0000

    feat: animate modal open

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,4 +1,14 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+    from { opacity: 0; }
+    to { opacity: 1; }
+`;
+
+const scaleIn = keyframes`
+    from { transform: scale(0); }
+    to { transform: scale(1); }
+`;
 
 export const Overlay = styled.div`
     width: 100%;
@@ -15,6 +25,8 @@ export const Overlay = styled.div`
     align-items: center;
     justify-content: center;
 
+    animation: ${fadeIn} 0.3s;
+
 `;
 
 export const Container = styled.div`
@@ -26,6 +38,8 @@ export const Container = styled.div`
 
     background: #fff;
     box-shadow: rgba(0, 0, 0, 0.04);
+
+    animation: ${scaleIn} 0.3s;
 
     > h1 {
         font-size: 22px;
