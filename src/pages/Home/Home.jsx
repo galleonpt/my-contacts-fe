6b7c5b9c061b530aaf56021fb75a2b7898b842e0
galleonpt <jose15.luis00@gmail.com>
2023-12-26 18:34:18 +0000

    refactor: add useHome hook and use it on Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-nested-ternary */
-import {
-  useEffect, useState, useMemo, useCallback,
-} from 'react';
+
 import { Link } from 'react-router-dom';
 import {
   Container,
@@ -25,89 +23,28 @@ import magnifierQuestion from '../../assets/images/magnifier-question.svg';
 import Loader from '../../components/Loader/Loader';
 import Button from '../../components/Button/Button';
 import Modal from '../../components/Modal/Modal';
-import ContactsService from '../../services/ContactsService';
-
-import toast from '../../utils/toast';
+import useHome from './useHome';
 
 function Home() {
-  const [contacts, setContacts] = useState([]);
-  const [orderBy, setOrderBy] = useState('asc');
-  const [searchName, setSearchName] = useState('');
-  const [isLoading, setIsLoading] = useState(true);
-  const [hasError, setHasError] = useState(false);
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [contactToDelete, setContactToDelete] = useState(null);
-  const [isDeleteLoading, setIsDeleteLoading] = useState(false);
+  const {
+    isLoading,
+    isDeleteModalOpen,
+    isDeleteLoading,
+    hasError,
 
-  const filteredContacts = useMemo(() => contacts.filter((contact) => (
-    contact.name.toLowerCase().includes(searchName.toLowerCase())
-  )), [searchName, contacts]);
+    contacts,
+    contactToDelete,
+    searchName,
+    filteredContacts,
+    orderBy,
 
-  // ! handlers
-  const fetchContacts = useCallback(async () => {
-    try {
-      setIsLoading(true);
-
-      const contactsList = await ContactsService.list(orderBy);
-
-      setContacts(contactsList);
-      setHasError(false);
-    } catch {
-      setHasError(true);
-    } finally {
-      setIsLoading(false);
-    }
-  }, [orderBy]);
-
-  const handleToggleOrderBy = () => {
-    setOrderBy((prev) => (prev === 'asc' ? 'desc' : 'asc'));
-  };
-
-  const handleChangeSearchName = (event) => {
-    setSearchName(event.target.value);
-  };
-
-  const handleTryAgain = () => {
-    fetchContacts();
-  };
-
-  const handleDeleteContact = (contact) => {
-    setContactToDelete(contact);
-    setIsDeleteModalOpen(true);
-  };
-
-  const handleCloseDeleteContact = () => {
-    setIsDeleteModalOpen(false);
-    setContactToDelete(null);
-  };
-
-  const handleConfirmDeleteContact = async () => {
-    try {
-      setIsDeleteLoading(true);
-      await ContactsService.delete(contactToDelete.id);
-
-      setContacts((prev) => prev.filter((contact) => contact.id !== contactToDelete.id));
-
-      handleCloseDeleteContact();
-
-      toast({
-        type: 'success',
-        text: 'Contact deleted successfully',
-        duration: 3,
-      });
-    } catch {
-      toast({
-        type: 'danger',
-        text: 'Error deleting contact',
-        duration: 3,
-      });
-    } finally { setIsDeleteLoading(false); }
-  };
-
-  // ! effects
-  useEffect(() => {
-    fetchContacts();
-  }, [orderBy, fetchContacts]);
+    handleCloseDeleteContact,
+    handleConfirmDeleteContact,
+    handleChangeSearchName,
+    handleTryAgain,
+    handleToggleOrderBy,
+    handleDeleteContact,
+  } = useHome();
 
   // ! render
   return (
---
+++ src/pages/Home/useHome.js
@@ -0,0 +1,109 @@
+import {
+  useEffect, useState, useMemo, useCallback,
+} from 'react';
+import ContactsService from '../../services/ContactsService';
+
+import toast from '../../utils/toast';
+
+const useHome = () => {
+  const [contacts, setContacts] = useState([]);
+  const [orderBy, setOrderBy] = useState('asc');
+  const [searchName, setSearchName] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [contactToDelete, setContactToDelete] = useState(null);
+  const [isDeleteLoading, setIsDeleteLoading] = useState(false);
+
+  const filteredContacts = useMemo(() => contacts.filter((contact) => (
+    contact.name.toLowerCase().includes(searchName.toLowerCase())
+  )), [searchName, contacts]);
+
+  // ! handlers
+  const fetchContacts = useCallback(async () => {
+    try {
+      setIsLoading(true);
+
+      const contactsList = await ContactsService.list(orderBy);
+
+      setContacts(contactsList);
+      setHasError(false);
+    } catch {
+      setHasError(true);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [orderBy]);
+
+  const handleToggleOrderBy = () => {
+    setOrderBy((prev) => (prev === 'asc' ? 'desc' : 'asc'));
+  };
+
+  const handleChangeSearchName = (event) => {
+    setSearchName(event.target.value);
+  };
+
+  const handleTryAgain = () => {
+    fetchContacts();
+  };
+
+  const handleDeleteContact = (contact) => {
+    setContactToDelete(contact);
+    setIsDeleteModalOpen(true);
+  };
+
+  const handleCloseDeleteContact = () => {
+    setIsDeleteModalOpen(false);
+    setContactToDelete(null);
+  };
+
+  const handleConfirmDeleteContact = async () => {
+    try {
+      setIsDeleteLoading(true);
+      await ContactsService.delete(contactToDelete.id);
+
+      setContacts((prev) => prev.filter((contact) => contact.id !== contactToDelete.id));
+
+      handleCloseDeleteContact();
+
+      toast({
+        type: 'success',
+        text: 'Contact deleted successfully',
+        duration: 3,
+      });
+    } catch {
+      toast({
+        type: 'danger',
+        text: 'Error deleting contact',
+        duration: 3,
+      });
+    } finally { setIsDeleteLoading(false); }
+  };
+
+  // ! effects
+  useEffect(() => {
+    fetchContacts();
+  }, [orderBy, fetchContacts]);
+
+  return {
+    isLoading,
+    isDeleteModalOpen,
+    isDeleteLoading,
+    hasError,
+
+    contacts,
+    contactToDelete,
+    searchName,
+    filteredContacts,
+    orderBy,
+
+    handleCloseDeleteContact,
+    handleConfirmDeleteContact,
+    handleChangeSearchName,
+    handleTryAgain,
+    handleToggleOrderBy,
+    handleDeleteContact,
+  };
+};
+
+export default useHome;
