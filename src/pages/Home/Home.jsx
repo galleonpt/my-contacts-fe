ca2cdc6ea7e93f1fa6ec13c3ff856eb9c7d68dca
galleonpt <jose15.luis00@gmail.com>
2023-12-26 22:16:37 +0000

    refactor: move ContactsList  to a new component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,14 @@
-import { Link } from 'react-router-dom';
-import {
-  Container,
-  ListHeader,
-  Card,
-} from './styles';
-import arrow from '../../assets/images/icons/arrow.svg';
-import edit from '../../assets/images/icons/edit.svg';
-import trash from '../../assets/images/icons/trash.svg';
+import { Container } from './styles';
 
 import Loader from '../../components/Loader/Loader';
-import Modal from '../../components/Modal/Modal';
 import useHome from './useHome';
 import InputSearch from './components/InputSearch/InputSearch';
 import Header from './components/Header/Header';
 import ErrorStatus from './components/ErrorStatus/ErrorStatus';
 import EmptyList from './components/EmptyList/EmptyList';
 import SearchNotFound from './components/SearchNotFound/SearchNotFound';
+import ContactsList from './components/ContactsList/ContactsList';
+import Modal from '../../components/Modal/Modal';
 
 function Home() {
   const {
@@ -65,39 +58,17 @@ function Home() {
             <SearchNotFound searchName={searchName} />
           )}
 
-          {filteredContacts.length > 0 && (
-          <ListHeader orderBy={orderBy}>
-            <button type="button" onClick={handleToggleOrderBy}>
-              <span>Name</span>
-
-              <img src={arrow} alt="Arrow" />
-            </button>
-          </ListHeader>
-          )}
-
-          {filteredContacts.map((contact) => (
-            <Card key={contact.id}>
-              <div className="info">
-                <div className="contact-name">
-                  <strong>{contact.name}</strong>
-                  { contact.category.name && <small>{contact.category.name}</small> }
-                </div>
-
-                <span>{contact.email}</span>
-                <span>{contact.phone}</span>
-              </div>
-
-              <div className="actions">
-                <Link to={`/edit/${contact.id}`}>
-                  <img src={edit} alt="Edit" />
-                </Link>
-
-                <button type="button" onClick={() => handleDeleteContact(contact)}>
-                  <img src={trash} alt="Delete" />
-                </button>
-              </div>
-            </Card>
-          ))}
+          <ContactsList
+            isDeleteModalOpen={isDeleteModalOpen}
+            isDeleteLoading={isDeleteLoading}
+            filteredContacts={filteredContacts}
+            contactToDelete={contactToDelete}
+            orderBy={orderBy}
+            onCloseDeleteContact={handleCloseDeleteContact}
+            onConfirmDeleteContact={handleConfirmDeleteContact}
+            onToggleOrderBy={handleToggleOrderBy}
+            onDeleteContact={handleDeleteContact}
+          />
 
           <Modal
             danger
