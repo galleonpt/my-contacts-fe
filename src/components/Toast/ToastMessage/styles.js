b71814853238cf4013e6d4d9d20bc4007fa08f9b
galleonpt <jose15.luis00@gmail.com>
2023-12-27 03:03:27 +0000

    feat: animate ToastMessage mount

--- src/components/Toast/ToastMessage/styles.js
+++ src/components/Toast/ToastMessage/styles.js
@@ -1,4 +1,16 @@
-import styled, { css } from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
+
+const messageIn = keyframes`
+    from {
+        opacity: 0;
+        transform: translateY(100px);
+    }
+
+    to {
+        opacity: 1;
+        transform: translateY(0px);
+    }
+`;
 
 const containerVariants = {
   default: css`
@@ -28,6 +40,8 @@ export const Container = styled.div`
     align-items: center;
 
     gap: 8px;
+
+    animation: ${messageIn} .3s;
 
     & + & {
         margin-top: 12px;
