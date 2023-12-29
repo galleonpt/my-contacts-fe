0d25de4182503a9b662abee5a7656fd556977b62
galleonpt <jose15.luis00@gmail.com>
2023-12-29 18:17:01 +0000

    refactor: change routes to useRoutes

--- src/router.jsx
+++ src/router.jsx
@@ -1,16 +1,16 @@
-import { Routes, Route } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import NewContact from './pages/NewContact/NewContact';
 import Edit from './pages/EditContact/EditContact';
 
 function Router() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/new" element={<NewContact />} />
-      <Route path="/edit/:id" element={<Edit />} />
-    </Routes>
-  );
+  const router = useRoutes([
+    { path: '/', element: <Home /> },
+    { path: '/new', element: <NewContact /> },
+    { path: '/edit/:id', element: <Edit /> },
+  ]);
+
+  return router;
 }
 
 export default Router;
